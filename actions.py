323dc98cc1e68034c0b21b848291a0def87ea3d1
simonet.joris <simonet.joris@gmail.com>
2018-01-06 12:57:07 +0100

    Mise en place de l'observation d'une paire, Création de buy et sell order.

---
+++ actions.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+
+def observation(api, pair):
+    pair = pair.replace("/", "_")
+
+    libelles_coin = pair.split("_", 1)
+
+    while True:
+        market, error = api.get_market(pair)
+        if error is not None:
+            print "L'erreur suivante a été retournée : \n" + error
+        else:
+            print 'Dernier prix : {:.8f} {}'.format(market['LastPrice'], libelles_coin[1])
+            print "Prix d'achat : {:.8f} {}".format(market['BidPrice'], libelles_coin[1])
+            print 'Prix de vente : {:.8f} {}'.format(market['AskPrice'], libelles_coin[1])
+            print 'High : {:.8f} {}'.format(market['High'], libelles_coin[1])
+            print 'Low : {:.8f} {}'.format(market['Low'], libelles_coin[1])
+            print 'Change :', market['Change'], '%'
+
+        time.sleep(5)
+
+
+def buy_order(api, pair):
+    pair = pair.replace("/", "_")
+
+    libelles_coin = pair.split("_", 1)
+
+    balance_achat, error = api.get_balance(libelles_coin[1])
+    if error is not None:
+        print "L'erreur suivante a été retournée : \n" + error
+        disponibilite_achat = 0
+    else:
+        disponibilite_achat = balance_achat['Available']
+
+    print 'Vous avez', disponibilite_achat, libelles_coin[1], 'disponible(s).'
+
+    if disponibilite_achat != 0:
+        print 'Vous ne pouvez pas effectuer de buy order'
+        return
+    else:
+        correct = False
+        while not correct:
+            try:
+                balance_utilisee = float(
+                    raw_input("Combien de {} voulez-vous utiliser ? : ".format(libelles_coin[1])))
+                correct = True
+            except ValueError:
+                print 'Veuillez entrer un nombre !'
+
+        while balance_utilisee > disponibilite_achat:
+            print 'Vous ne pouvez utiliser plus de {}'.format(disponibilite_achat)
+            correct = False
+            while not correct:
+                try:
+                    balance_utilisee = float(
+                        raw_input("Combien de {} voulez-vous utiliser ? : ".format(libelles_coin[1])))
+                    correct = True
+                except ValueError:
+                    print 'Veuillez entrer un nombre !'
+
+        marche_paire, error = api.get_market(pair)
+        if error is not None:
+            print "L'erreur suivante a été retournée : \n" + error
+            return
+
+        print "L'ordre de vente le moins cher est de {:.8f} \n".format(marche_paire['AskPrice'])
+
+        correct = False
+        while not correct:
+            try:
+                bid_price = float(raw_input("Quel est votre prix ? : "))
+                correct = True
+            except ValueError:
+                print 'Veuillez entrer un nombre !'
+
+        # prise en compte des fees de Cryptopia pour calculé le nombre de coins achetable
+        num_coins = (balance_utilisee * (balance_utilisee * 0.00201)) / bid_price
+        prix_achat = bid_price * num_coins
+
+        trade, error = api.submit_trade(pair.replace("_", "/"), 'Buy', bid_price, num_coins)
+        if error is not None:
+            print "L'erreur suivante a été retournée : \n" + error
+            return
+
+        print trade
+
+        print "\n[+] Ordre d'achat placé pour {:.8f} {} coins à {:.8f} {} \
+               chacun pour un total de {} {}".format(num_coins, libelles_coin[0], bid_price, libelles_coin[1], prix_achat, libelles_coin[1])
+
+
+def sell_order(api, pair):
+    pair = pair.replace("/", "_")
+
+    libelles_coin = pair.split("_", 1)
+
+    balance_vente, error = api.get_balance(libelles_coin[0])
+    if error is not None:
+        print "L'erreur suivante a été retournée : \n" + error
+        disponibilite_vente = 0
+    else:
+        disponibilite_vente = balance_vente['Available']
+
+    print 'Vous avez', disponibilite_vente, libelles_coin[0], 'disponible(s).'
+
+    if disponibilite_vente == 0:
+        print 'Vous ne pouvez pas effectuer de sell order'
+        return
+    else:
+        while not correct:
+            try:
+                balance_utilisee = float(
+                    raw_input("Combien de {} voulez-vous utiliser ? : ".format(libelles_coin[0])))
+                correct = True
+            except ValueError:
+                print 'Veuillez entrer un nombre !'
+
+        while balance_utilisee > disponibilite_vente:
+            print 'Vous ne pouvez utiliser plus de {}'.format(disponibilite_vente)
+            correct = False
+            while not correct:
+                try:
+                    balance_utilisee = float(
+                        raw_input("Combien de {} voulez-vous utiliser ? : ".format(libelles_coin[0])))
+                    correct = True
+                except ValueError:
+                    print 'Veuillez entrer un nombre !'
+
+        marche_paire, error = api.get_market(pair)
+        if error is not None:
+            print "L'erreur suivante a été retournée : \n" + error
+            return
+
+        print "L'ordre d'achat le plus cher est de {:.8f} \n".format(marche_paire['BidPrice'])
+
+        correct = False
+        while not correct:
+            try:
+                ask_price = float(raw_input("Quel est votre prix ? : "))
+                correct = True
+            except ValueError:
+                print 'Veuillez entrer un nombre !'
+
+        # prise en compte des fees de Cryptopia pour calculé le nombre de coins vendable
+        num_coins = (balance_utilisee * (balance_utilisee * 0.00201)) / ask_price
+        prix_vente = ask_price * num_coins
+
+        trade, error = api.submit_trade(pair.replace("_", "/"), 'Sell', ask_price, num_coins)
+        if error is not None:
+            print "L'erreur suivante a été retournée : \n" + error
+            return
+
+        print trade
+
+        print "\n[+] Ordre de vente placé pour {:.8f} {} coins à {:.8f} {} \
+               chacun pour un total de {} {}".format(num_coins, libelles_coin[0], ask_price, libelles_coin[1], prix_vente, libelles_coin[1])
---
+++ main.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import signal
+import sys
+from cryptopia_api import Api
+import actions
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# Get these from (link here)
+def get_secret(secret_file):
+    """Grabs API key and secret from file and returns them"""
+
+    with open(secret_file) as secrets:
+        secrets_json = json.load(secrets)
+        secrets.close()
+
+    return str(secrets_json['key']), str(secrets_json['secret'])
+
+
+def sigint_handler():
+    """Handler for ctrl+c"""
+    print '\n[!] CTRL+C pressed. Exiting...'
+    sys.exit(0)
+
+
+EXIT_CYCLE = False
+while not EXIT_CYCLE:
+
+    # setup api
+    KEY, SECRET = get_secret("secrets.json")
+    API = Api(KEY, SECRET)
+
+    COIN = raw_input("Choisissez le coin à observer : \
+           \nSoyez précis !\nEcrivez en majuscule (ex : WISH)\n")
+
+    # do before entering coin to save the API call during the pump
+    BALANCE, ERROR = API.get_balance(COIN)
+    if ERROR is not None:
+        print "L'erreur suivante a été retournée : \n" + ERROR
+        AVAILABLE = 0
+
+
+    # AVAILABLE = BALANCE['Available']
+
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    ALLOW_ORDERS = True
+
+    print 'Vous avez', AVAILABLE, COIN, 'disponible(s).'
+
+    PAIRS, ERROR = API.get_tradepairs()
+
+    print 'Choisissez votre paire :'
+    index = 0
+    LISTE_PAIRES = []
+
+    for PAIR in PAIRS:
+        if PAIR['Status'] == 'OK' and COIN + "/" in PAIR['Label']:
+            LISTE_PAIRES.append(PAIR['Label'])
+            print index, ":", PAIR['Label']
+            index += 1
+
+    if index == 0:
+        print "Aucune paire tradable n'a été trouvé."
+        break
+
+    while True:
+        correct = False
+        while not correct:
+            try:
+                INDEX_PAIRE = int(raw_input("Votre choix : "))
+                correct = True
+            except ValueError:
+                print 'Veuillez entrer un chiffre en 0 et', index-1, '!'
+
+        if len(LISTE_PAIRES) > INDEX_PAIRE:
+            PAIR = LISTE_PAIRES[INDEX_PAIRE]
+            break
+
+    liste_actions = {0: actions.observation,
+            1: actions.buy_order,
+            2: actions.sell_order}
+
+    while True:
+        correct = False
+        while not correct:
+            try:
+                ACTION = int(raw_input("Quelle action voulez-vous réaliser ? : \
+                            \n0: Observation du cours. \
+                            \n1: Faire un buy order. \
+                            \n2: Faire un sell order. \
+                            \nVotre choix : "))
+                correct = True
+            except ValueError:
+                print 'Veuillez entrer un chiffre en 0 et 2 !'
+
+        if len(liste_actions) > ACTION:
+            liste_actions[ACTION](API, PAIR)
+            break
+
+    if __name__ == "__main__":
+        ANSWER = raw_input("\nVoulez-vous relancer le bot ? (y/n) ")
+        if ANSWER.lower().strip() in "n no".split():
+            EXIT_CYCLE = True
